5701f9440b3985e0dd417a07f6214f9222b0781b
keshabbhattarai126 <keshab126bhattarai@gmail.com>
2025-04-23 12:51:31 +0545

    first commit

---
+++ Question1.java
@@ -0,0 +1,10 @@
+package Lesson5Task;
+
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+    }
+    
+}
