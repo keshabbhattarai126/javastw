409ce9f03a3b25360c36637508d6c08af5f20478
keshabbhattarai126 <keshab126bhattarai@gmail.com>
2025-07-04 16:43:22 +0545

    set4

---
+++ testt.java
@@ -0,0 +1,11 @@
+package Lesson5Task;
+
+public class testt {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+        System.out.println(10 + 20 + "Java\n"+ a + b);
+    }
+    
+}
